240a253a38f231edeb5bd48cdc5f515a7fde55c4
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2024-11-26 01:44:06 +0000

    Made the solution more optimal.
    Removed the unwanted funcions and commented code.
    
    Added necessary comments to the solution

--- Medium/Nearest_Exit_from_Entrance_in_Maze/solution.py
+++ Medium/Nearest_Exit_from_Entrance_in_Maze/solution.py
@@ -1,101 +1,45 @@
 from collections import deque
 class Solution:
-    def getExits(self,maze:List[List[str]]) -> List[List[int]]:
-        exits = []
-        n = len(maze[0])
-        m = len(maze)
-        for i in range(m):
-            if maze[i][0] == '.':
-                exits.append([i,0])
-            if maze[i][n-1] == '.':
-                exits.append([i,n-1])
-        
-        for j in range(1,n-1):
-            if maze[0][j] == '.':
-                exits.append([0,j])
-            
-            if maze[m-1][j] == '.':
-                exits.append([m-1,j])
+    
 
-        
-        return exits
-
-    def getNeighbours(self,maze,i,j):
-        neighbours = []
-        if i > 0 and maze[i-1][j] == '.':  #Move UP
-            neighbours.append([i-1,j])
-        
-        if i < len(maze) - 1 and maze[i+1][j] == '.': # Move Down
-            neighbours.append([i+1,j])
-        
-        if j > 0 and maze[i][j-1] == '.': # Move left
-            neighbours.append([i,j-1])
-        
-        if j < len(maze[0]) - 1 and maze[i][j+1] == '.':
-            neighbours.append([i,j+1])
-        
-
-        return neighbours
 
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
 
        
+        #Initialize the Variables
         m,n = len(maze),len(maze[0])
         entrance_row,entrance_column = entrance
         directions = [(-1,0),(1,0),(0,-1),(0,1)]
 
 
-        exits = self.getExits(maze)
-        
-        if entrance in exits:
-            exits.remove(entrance)
-        
-        if len(exits) == 0:
-            return -1 
-        
- 
-
-
-
-        #Initialize Length Matrix and Queue
-        # min_length = [[-1] * n) for _ in range(m)]
         queue = deque()
 
 
         #Setting Entrance Values
         queue.append((entrance_row,entrance_column,0))
-        # min_length[entrance[0]][entrance[1]] = 0
+        maze[entrance_row][entrance_column] = '+'
+        
 
 
 
-        # print(min_length)
+
 
         while queue:
 
-            # print(min_length)
-            i,j,dist = queue.popleft() #get current position
-
-            # current_neighbours = self.getNeighbours(maze,i,j)
-            # print(current_neighbours)
-
-
+            i,j,dist = queue.popleft() #get current position and distance
 
             for d in directions:
                 
-                x = i + d[0]
-                y = j + d[1]
-
-                if [x,y] in exits:
-                    
-                    return dist + 1
+                x,y = i + d[0],j + d[1]   # Getting the neighbour points 
                 
-                if  0 <= x < m and 0 <= y < n and maze[x][y] == '.':
-                    # min_length[neighbour[0]][neighbour[1]] = min_length[i][j] + 1
+                if  0 <= x < m and 0 <= y < n and maze[x][y] == '.': # Check if neighbour points are valid and not a wall
+                    if x == 0 or x == m-1 or y == 0 or y == n-1: # Check if the neighbour point is an exit
+                        return dist + 1
 
-                    maze[x][y] = '+'
-                    queue.append((x,y,dist+1))
+                    maze[x][y] = '+' # If not an exit, we mark the neighbour as visited (with making it wall)
+                    queue.append((x,y,dist+1)) # Now adding it to the queue to be tranversed later
 
                 
 
       
-        return -1 
+        return -1 # If the queue ends with no more traversals, it means no exit can be reached. 
