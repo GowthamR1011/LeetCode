11726cf8e6b766cf71fe978daf6036bbd9a010f2
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2025-05-06 15:24:46 +0530

    Updated solution for the problem of build arrays from leetcode.

--- Easy/Build Array from Permutation/Solution.py
+++ Easy/Build Array from Permutation/Solution.py
@@ -1,8 +1,3 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        new_list = []
-
-        for i in range(len(nums)):
-            new_list.append(nums[nums[i]])
-
-        return new_list
+        return [nums[nums[i]] for i in range(len(nums))]
