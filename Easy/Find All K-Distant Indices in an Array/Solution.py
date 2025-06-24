e75c7631fd2f96573b86f0fa205f1f36c3ed1133
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2025-06-24 13:41:08 +0100

    Solution for Find All K-Distant Indices in an Array problem from LeetCode

---
+++ Easy/Find All K-Distant Indices in an Array/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
+        
+        n = len(nums)
+        res = []
+        r = 0
+
+        for j in range(n):
+            if nums[j] == key:
+                l = max(r,j-k)
+                r = min(n- 1, j + k) + 1
+                for i in range(l, r):
+                    res.append(i)
+                    
+        return res
+
+            
